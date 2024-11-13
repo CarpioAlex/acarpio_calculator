8a98ae862a8ffa36425b0f63dedf8b2e82be6ea6
Alex <acarpiomoratalla@cifpfbmoll.eu>
2024-11-13 12:30:41 +0100

    Added drawables

--- app/src/main/java/com/acarpio/acarpio_calculator/CalculatorListener.java
+++ app/src/main/java/com/acarpio/acarpio_calculator/CalculatorListener.java
@@ -2,25 +2,52 @@ package com.acarpio.acarpio_calculator;
 
 import android.view.View;
 import android.widget.Button;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import com.acarpio.acarpio_calculator.databinding.ActivityMainBinding;
 
-public class CalculatorListener implements View.OnClickListener  {
+import java.util.ArrayList;
+
+public class CalculatorListener implements View.OnClickListener {
 
     private ActivityMainBinding binding;
     private TextView numberContainer;
+    private TextView currentHistory;
+    private ScrollView historyContainer;
+    private TextView historyText;
     private StringBuilder input = new StringBuilder();
-    ;
+    private StringBuilder history = new StringBuilder();
+
+    private ArrayList<StringBuilder> historyList = new ArrayList<>();
+
+    private double operand1 = 0;
+    private double operand2 = 0;
+    private double memory = 0;
+    private String operator = "";
+    private boolean isNewInput = true;
+    private boolean historyOpened = false;
 
     public CalculatorListener(ActivityMainBinding binding) {
         this.binding = binding;
-        this.numberContainer = binding.numberContainer;
         this.input = MainActivity.getCurrentInput();
+        this.numberContainer = binding.numberContainer;
+        this.currentHistory = binding.currentHistory;
+        this.historyContainer = binding.historyContainer;
+        this.historyText = binding.historyText;
     }
+
+
     @Override
     public void onClick(View v) {
         input = MainActivity.getCurrentInput();
+
+
+        if (isNewInput) {
+            input.setLength(0);
+
+            isNewInput = false;
+        }
 
         if (v == binding.b00) {
             input.append("00");
@@ -30,32 +57,181 @@ public class CalculatorListener implements View.OnClickListener  {
             input.append("1");
         } else if (v == binding.b2) {
             input.append("2");
-            } else if (v == binding.b3) {
+        } else if (v == binding.b3) {
             input.append("3");
         } else if (v == binding.b4) {
             input.append("4");
-            } else if (v == binding.b5) {
+        } else if (v == binding.b5) {
             input.append("5");
         } else if (v == binding.b6) {
             input.append("6");
-            } else if (v == binding.b7) {
+        } else if (v == binding.b7) {
             input.append("7");
         } else if (v == binding.b8) {
             input.append("8");
-            } else if (v == binding.b9) {
+        } else if (v == binding.b9) {
             input.append("9");
-        }
+        } else if (v == binding.suma) {
+            setOperator("+");
+        } else if (v == binding.resta) {
+            setOperator("-");
+        } else if (v == binding.division) {
+            setOperator("/");
+        } else if (v == binding.multiplication) {
+            setOperator("*");
+        } else if (v == binding.equals) {
+            setResult();
+        } else if (v == binding.buttonDelete) {
+            if (input.length() > 0) {
+                input.setLength(input.length() - 1);
+                updateInput();
+            }
+        } else if (v == binding.buttonClear) {
+            input.setLength(0);
+            updateInput();
+        } else if (v == binding.buttonMS) {
+            saveMemory();
+        } else if (v == binding.buttonMR) {
+            if (memory != 0) {
+                input.append(memory);
+                updateInput();
+            }
+        } else if (v == binding.buttonMPlus) {
+            addMemory();
+        } else if (v == binding.buttonMLess) {
+            restarMemory();
+        } else if (v == binding.buttonMC) {
+            memory = 0;
+        } else if (v == binding.currentHistory) {
+            if (!historyOpened) {
+                historyContainer.setVisibility(View.VISIBLE);
+                populateHistory();
+            } else {
+                historyContainer.setVisibility(View.GONE);
+                historyOpened = false;
+            }
 
+        }
 
         updateInput();
     }
 
-
-
-
     public void updateInput() {
         binding.numberContainer.setText(input.toString());
+        binding.currentHistory.setText(history.toString());
+
     }
+
+    public void resetHistory() {
+        history.setLength(0);
+        updateInput();
+    }
+
+    public void setOperator(String op) {
+        if (input.length() > 0) {
+            if (!operator.isEmpty()) {
+
+                setResult();
+            }
+
+            if (operator.isEmpty()) {
+                resetHistory();
+            }
+
+            operand1 = Double.parseDouble(input.toString());
+            operator = op;
+            history.append(operand1).append(" ").append(operator).append(" ");
+            updateInput();
+            input.setLength(0);
+            isNewInput = true;
+        }
+    }
+
+    public void setResult() {
+        if (input.length() > 0 && !operator.isEmpty()) {
+            operand2 = Double.parseDouble(input.toString());
+            double result;
+
+            switch (operator) {
+                case "+":
+                    result = operand1 + operand2;
+                    break;
+                case "-":
+                    result = operand1 - operand2;
+                    break;
+                case "*":
+                    result = operand1 * operand2;
+                    break;
+                case "/":
+                    if (operand2 != 0) {
+                        result = operand1 / operand2;
+                    } else {
+                        binding.numberContainer.setText(">:(");
+                        return;
+                    }
+                    break;
+                default:
+                    return;
+            }
+            String resultString = operand1 + " " + operator + " " + operand2 + " = " + result;
+            history.append(operand2).append(" = ").append(result);
+            historyList.add(new StringBuilder(resultString));
+            input.setLength(0);
+            input.append(result);
+            operand1 = result;
+
+
+            updateInput();
+            operator = "";
+            isNewInput = true;
+
+            if (historyOpened) {
+                populateHistory();
+            }
+
+        }
+    }
+
+    // Memory
+
+    public void saveMemory() {
+        if (input.length() > 0) {
+            memory = Double.parseDouble(input.toString());
+        }
+    }
+
+    public void addMemory() {
+        if (input.length() > 0) {
+            memory += Double.parseDouble(input.toString());
+        }
+
+    }
+
+    public void restarMemory() {
+        if (input.length() > 0) {
+            memory -= Double.parseDouble(input.toString());
+        }
+    }
+
+
+    // History
+
+    public void populateHistory() {
+
+
+
+        historyOpened = true;
+        historyText.setText("");
+
+
+        for (StringBuilder hist : historyList) {
+            historyText.append(hist.toString() + "\n");
+        }
+
+
+        historyContainer.post(() -> historyContainer.fullScroll(View.FOCUS_DOWN));
+    }
+
 
 
 }
--- app/src/main/java/com/acarpio/acarpio_calculator/MainActivity.java
+++ app/src/main/java/com/acarpio/acarpio_calculator/MainActivity.java
@@ -4,24 +4,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.content.res.AppCompatResources;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.acarpio.acarpio_calculator.databinding.ActivityMainBinding;
 
+
 public class MainActivity extends AppCompatActivity {
 
     public static StringBuilder input;
-    String operator;
-    Double result;
-
-    boolean numberInMemory;
-    double memoryValue;
-
-
 
 
 
@@ -46,10 +42,9 @@ public class MainActivity extends AppCompatActivity {
         assignListeners(binding, listener);
 
         input = new StringBuilder();
-        operator = "";
-        result = 0.0;
-        numberInMemory = false;
-        memoryValue = 0.0;
+
+
+
 
     }
 
@@ -58,9 +53,23 @@ public class MainActivity extends AppCompatActivity {
             View child = binding.gridCalc.getChildAt(i);
             if (child instanceof Button) {
                 child.setOnClickListener(listener);
+                child.setBackground(AppCompatResources.getDrawable(this, R.drawable.selector_boton));
             }
         }
+        for (int i = 0; i < binding.secondGrid.getChildCount(); i++) {
+            View child = binding.secondGrid.getChildAt(i);
+            if (child instanceof Button) {
+                child.setOnClickListener(listener);
+                child.setBackground(AppCompatResources.getDrawable(this, R.drawable.selector_boton));
+            }
+        }
+
+        binding.currentHistory.setOnClickListener(listener);
+
     }
+
+
+
 
     public static StringBuilder getCurrentInput() {
         return MainActivity.input;
